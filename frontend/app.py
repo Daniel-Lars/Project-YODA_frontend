6a01b21913d1ff271804c5b408cb06c49f56af28
Jakob Steininger <steiningerjakob@gmail.com>
2021-12-01 22:32:05 +0100

    Refine frontend and connect to API

--- frontend/app.py
+++ frontend/app.py
@@ -1,12 +1,21 @@
 from google.cloud import storage
-
-import streamlit as st
+from PIL import Image
+from random import choice
+from time import sleep
 import requests
+import streamlit as st
 
 BUCKET_NAME = 'wagon-data-745-project-yoda'
 DESTINATION_BLOB_NAME = 'images/image.jpg'
+URL_PROD = 'https://projectyoda-zl47dkr23a-ew.a.run.app/predict'
+URL_DEV = 'http://34.140.153.26:5000/predict'
+
+url = URL_PROD
 
 
+# helper functions:
+
+## upload image to gcp
 def upload_blob(file):
     """Uploads a file to the bucket."""
 
@@ -18,19 +27,62 @@ def upload_blob(file):
 
 st.header('Project Yoda ')
 
-# File upload function:
+## show Yoda with wise comments
+def show_yoda(col1, col2):
+    quotes = [
+        'When you look at the dark side, careful you must be. For the dark side looks back.',
+        'If no mistake you have made, losing you are. A different game you should play.',
+        'You must unlearn what you have learned.',
+        'Size matters not. Look at me. Judge me by my size, do you?',
+        'Try not. Do. Or do not. There is no try.',
+        'Train yourself to let go of everything you are afraid to lose.',
+        'Named must your fear be before banish it you can.'
+    ]
 
+    yoda = Image.open('./yoda.png')
+    col1.image(yoda, width=128)
+
+    text1 = 'Hmm.. the Force I must use to predict your image..'
+    col2.write(text1)
+    sleep(2)
+    col2.write('Some wisdom I will share in the meantime:')
+    sleep(2)
+    wisdom = choice(quotes)
+    col2.write(wisdom)
+
+## make prediction
+def make_prediction(col2):
+    result = requests.get(url)
+    result = result.json()
+    prediction = result['prediction']
+
+    if prediction == None:
+        pass
+    if prediction == 'YODA':
+        prediction = 'ME'
+    if prediction == 'DARTH VADER':
+        prediction = 'my old enemy DARTH VADER'
+    col2.markdown(f'Hmm.. yes.. **{prediction}** this is!')
+
+
+# for frontend rendering:
+
+## get image from user input
 test_image = st.file_uploader(label="Choose a file", type=['jpg','png'])
 
 if test_image != None:
-    st.image(test_image)
+    st.image(test_image, width=256)
     upload_blob(test_image)
 
 
-# Post request to prediction api
-
+## on button click, make GET request to prediction api
 if st.button('Classify me!'):
-    st.balloons()
-    result = requests.get('https://projectyoda-zl47dkr23a-ew.a.run.app/predict')
-    result = result.json()
-    st.write(result['prediction'])
+
+    col1, col2 = st.columns((1,3))
+
+    show_yoda(col1, col2)
+
+    try:
+        make_prediction(col2)
+    except:
+        st.write('Not able to get prediction..')
